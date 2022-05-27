b8a067850e8f2725e493878f83abfe8367c28d1f
Olliemint <furywrld5@gmail.com>
2022-05-27 15:26:31 +0300

    feat:creat template for single image

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('',views.gallery, name='gallery'),
+    path('photo/<str:id>/',views.get_image_by_id,name='single_photo'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def gallery(request):
     
     return render(request, 'gallery/gallery.html')
+
+
+def get_image_by_id(request,id):
+    
+    return render(request, 'gallery/gallery.html')
