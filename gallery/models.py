66e2d63724e7fb3a9c3f51ddfd3cb1eb790440f0
Olliemint <furywrld5@gmail.com>
2022-05-27 20:23:49 +0300

    feat:created category model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=80,null=False,blank=False)
+    
+    def __str__(self):
+        return self.category_name
+    
