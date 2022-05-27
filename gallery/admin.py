0b600f0a0215c8fd4b75ce45285c8ad12dcbc066
Olliemint <furywrld5@gmail.com>
2022-05-28 00:37:51 +0300

    feat: created admin panel

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import Location,Category,Image
 
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
+
