cd4df87b74abdf33922bbc351f684399bbdef0d1
Olliemint <furywrld5@gmail.com>
2022-05-27 20:36:10 +0300

    feat:created image model

--- gallery/models.py
+++ gallery/models.py
@@ -13,5 +13,15 @@ class Location(models.Model):
     location = models.CharField(max_length=80,null=False,blank=False)
     
     def __str__(self):
-        return self.location    
+        return self.location
+    
+    
+class Image(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL,null=True,blank=True)
+    name = models.CharField(max_length=80,null=False,blank=False)
+    description = models.TextField(max_length=700,null=False,blank=False)
+    
+    def __str__(self):
+        return self.description   
     
